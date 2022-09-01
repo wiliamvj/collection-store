7cfb517542b48f63c804d1cde6282c1a95f02318
wiliamvj <wiliamjoaquim@gmail.com>
2022-09-01 16:50:35 -0300

    add regex to validate a strong password

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+import { IsString, IsNotEmpty, IsEmail, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
@@ -11,5 +11,9 @@ export class CreateUserDto {
 
   @IsNotEmpty()
   @IsString()
+  @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/, {
+    message:
+      'Enter a password of at least 8 characters with capital letters and special characters',
+  })
   password: string;
 }
